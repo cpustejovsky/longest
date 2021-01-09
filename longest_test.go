7dc56200e7ea9ce0c1e865efa4e88c8275ce99b0
cpustejovsky <charles.pustejovsky@gmail.com>
2021-01-08 21:07:22 -0500

    Got tests failing

--- longest_test.go
+++ longest_test.go
@@ -1,20 +1,20 @@
-package longest
+package longest_test
 
 import (
 	"reflect"
 	"testing"
 
-	"github.com/cpustejovsky/gotest/longest"
+	"github.com/cpustejovsky/longest"
 )
 
-func testFindLongest(t *testing.T) {
+func TestFindLongest(t *testing.T) {
 	given := []string{"raccoon", "wildebeest", "newt", "termite", "tiger", "rat", "badger"}
 	want1 := []string{"badger", "raccoon", "newt", "tiger", "rat", "termite"}
 	want2 := []string{"badger", "rat", "tiger", "raccoon", "newt", "termite"}
 
 	got := longest.FindLongest(given)
 
-	if !(reflect.DeepEqual(got, want1) || reflect.DeepEqual(got, want2)) {
+	if !reflect.DeepEqual(got, want1) && !reflect.DeepEqual(got, want2) {
 		t.Errorf("\ngot:\n%v\n", got)
 	}
 }
