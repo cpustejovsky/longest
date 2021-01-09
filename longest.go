54b7fe18e7c42381c7f8ca2d1adf6c16a1a222ff
cpustejovsky <charles.pustejovsky@gmail.com>
2021-01-08 22:00:58 -0500

    Add Contains and Compare functions and unit tests

--- longest.go
+++ longest.go
@@ -2,5 +2,29 @@ package longest
 
 func FindLongest(words []string) []string {
 	var longest []string
+	for i, word := range words {
+		longest = append(longest, word)
+		for j, word2 := range words {
+			if i != j && Compare(word, word2) && !Contains(longest, word2) {
+				longest = append(longest, word2)
+			}
+		}
+	}
+
 	return longest
+}
+
+func Compare(first, last string) bool {
+	fl := string(first[len(first)-1])
+	ll := string(last[0])
+	return fl == ll
+}
+
+func Contains(l []string, s string) bool {
+	for _, v := range l {
+		if v == s {
+			return true
+		}
+	}
+	return false
 }
--- longest_test.go
+++ longest_test.go
@@ -7,6 +7,58 @@ import (
 	"github.com/cpustejovsky/longest"
 )
 
+func TestCompare(t *testing.T) {
+	tests := []struct {
+		name  string
+		first string
+		last  string
+		want  bool
+	}{
+		{"should return true", "world", "dog", true},
+		{"should return false", "hello", "world", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := longest.Compare(tt.first, tt.last)
+			if got != tt.want {
+				t.Errorf("\ngot:\n%v\nwanted:\n%v\n", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestContains(t *testing.T) {
+	tests := []struct {
+		name string
+		list []string
+		word string
+		want bool
+	}{
+		{
+			"should return true",
+			[]string{"badger", "raccoon", "newt", "tiger", "rat", "termite"},
+			"badger",
+			true,
+		},
+		{
+			"should return false",
+			[]string{"badger", "raccoon", "newt", "tiger", "rat", "termite"},
+			"world",
+			false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := longest.Contains(tt.list, tt.word)
+			if got != tt.want {
+				t.Errorf("\ngot:\n%v\nwanted:\n%v\n", got, tt.want)
+			}
+		})
+	}
+
+}
+
 func TestFindLongest(t *testing.T) {
 	given := []string{"raccoon", "wildebeest", "newt", "termite", "tiger", "rat", "badger"}
 	want1 := []string{"badger", "raccoon", "newt", "tiger", "rat", "termite"}
@@ -15,6 +67,6 @@ func TestFindLongest(t *testing.T) {
 	got := longest.FindLongest(given)
 
 	if !reflect.DeepEqual(got, want1) && !reflect.DeepEqual(got, want2) {
-		t.Errorf("\ngot:\n%v\n", got)
+		t.Errorf("\ngot:\n%v\nwanted:\n%v\nor\n%v", got, want1, want2)
 	}
 }
