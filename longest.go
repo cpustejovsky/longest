53ef04582a17109cf9b2e7ee20862a0ca9dfd448
cpustejovsky <charles.pustejovsky@gmail.com>
2021-01-25 20:50:15 -0500

    adding a bit more

--- longest.go
+++ longest.go
@@ -1,12 +1,7 @@
 package longest
 
-import "fmt"
-
 //Will need to create multiple possible combinations to piece together
 //The most efficient way to keep track of this is to use maps
-//For each word, check which words have matching first letter
-//Then see which of those have matching last letter
-//Keep coming up with longer and longer iterations doing this back and forth
 type matchingFirstLetter map[string][]string
 type matchingLastLetter map[string][]string
 
@@ -15,30 +10,36 @@ FindLongest takes a slice of strings and returns a subset of them
 (potentially the same length of the initial slice)
 with the last letter of the first string matched to the first letter of the next string
 */
+
+var fl = make(matchingFirstLetter)
+var ll = make(matchingLastLetter)
+
 func FindLongest(words []string) []string {
-	fmt.Println(words)
-	var longest []string
-	fl := make(matchingFirstLetter)
-	ll := make(matchingLastLetter)
 	for _, word := range words {
 		fl[string(word[0])] = append(fl[string(word[0])], word)
 		ll[string(word[len(word)-1])] = append(ll[string(word[len(word)-1])], word)
 	}
-	// for _, word := range words {
-	// 	longest = CreateNewLongest(word, words, longest)
-	// }
-	fmt.Println(fl)
-	fmt.Println(ll)
+	var longest []string
+	//range over words
+	for _, word := range words {
+		longest = CreateNewLongest(word, words, longest)
+	}
 	return longest
 }
 
 func CreateNewLongest(word string, words, longest []string) []string {
 	newLongest := []string{word}
-	for _, word2 := range words {
-		if Compare(word, word2) && !Contains(newLongest, word2) {
-
-			newLongest = append(newLongest, word2)
-			word = word2
+	last := len(newLongest) - 1
+	for i := 0; i < len(words); i++ {
+		l := string(newLongest[last][last])
+		arr, ok := fl[l]
+		if ok {
+			//at this point, it may make sense to make a recursive function, but I'm not sure how yet.
+			for _, word := range arr {
+				newLongest = append(newLongest, word)
+				//fix recursive solution to not do a stack overflow
+				return CreateNewLongest(string(newLongest[last][last]), words, longest)
+			}
 		}
 	}
 	if len(newLongest) >= len(longest) {
